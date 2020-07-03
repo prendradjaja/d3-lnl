dab12f8d5c024a18116353445f77ff43d088b4b7
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-07-03 15:27:23 -0700

    Add some missing index.htmls; Add some more meat to hello world

--- 1--hello-world/main.ts
+++ 1--hello-world/main.ts
@@ -1,7 +1,14 @@
-const message: string = "Hello, D3 and TypeScript!"
+const message: string = "From D3";
+d3.select('p')
+    .append('span')
+    .text(message);
 
-d3.select('body')
-  .append('p')
-  .text(message);
+d3.selectAll('#shopping-list li')
+    .style('color', 'green')
+  .append('input')
+    .attr('type', 'checkbox');
+
+d3.selectAll('#numbers li')
+  .text(() => Math.random());
 
 export {};
