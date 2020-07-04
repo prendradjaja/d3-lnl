842c1d605379b810b9cb2e0b2441efec1dced660
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-07-03 18:14:17 -0700

    Add more to hello world to emphasize method chaining

--- 1--hello-world/main.ts
+++ 1--hello-world/main.ts
@@ -5,7 +5,9 @@ d3.select('p')
 
 d3.selectAll('#shopping-list li')
     .style('color', 'green')
+    .style('font-family', 'sans-serif')
   .append('input')
+    .style('opacity', 0.5)
     .attr('type', 'checkbox');
 
 d3.selectAll('#numbers li')
