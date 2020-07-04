10f4577af11864c3ccb478339fe704bb118608f4
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-07-03 20:08:30 -0700

    1.empty now includes commented-out code

--- 1--hello-world.empty/main.ts
+++ 1--hello-world.empty/main.ts
@@ -1,1 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// d3.selectAll('#shopping-list li')
+//     .style('color', 'green')
+//     .style('font-family', 'sans-serif')
+//   .append('input')
+//     .style('opacity', 0.5)
+//     .attr('type', 'checkbox');
+
+// d3.selectAll('#numbers li')
+//     .text(() => Math.random());
+
 export {};
