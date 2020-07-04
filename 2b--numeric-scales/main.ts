3707739e805a5142d7b2fad9fa05d9a75ed8b5e1
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-07-03 21:42:37 -0700

    numeric++

--- 2b--numeric-scales/main.ts
+++ 2b--numeric-scales/main.ts
@@ -3,46 +3,46 @@ const points = [
   { x: 5, y: 80 },
 ];
 
-const width = 400;
-const height = 400;
+const width = 300;
+const height = 300;
 
 const margin = {
-  left: 100,
-  right: 100,
-  top: 100,
-  bottom: 100,
+  left: 50,
+  right: 30,
+  top: 30,
+  bottom: 50,
 };
 
 const svg =
   d3.select('body')
     .append('svg')
-      .attr('viewBox', `0 0 ${width} ${height}`)
       .attr('width', width)
+      .attr('height', height)
       .style('border', '1px solid gray');
 
-const x =
+const xScale =
   d3.scaleLinear()
     .domain([0, 10])
     .range([margin.left, width - margin.right]);
 
-const y =
+const yScale =
   d3.scaleLinear()
     .domain([0, 100])
     .range([height - margin.bottom, margin.top]);
 
 svg.append('g')
-  .call(d3.axisBottom(x))
+  .call(d3.axisBottom(xScale))
   .attr('transform', `translate(0, ${height - margin.bottom})`);
 
 svg.append('g')
-  .call(d3.axisLeft(y))
+  .call(d3.axisLeft(yScale))
   .attr('transform', `translate(${margin.left}, 0)`);
 
 for (let point of points) {
   svg.append('circle')
     .attr('r', 2)
-    .attr('cx', x(point.x))
-    .attr('cy', y(point.y));
+    .attr('cx', xScale(point.x))
+    .attr('cy', yScale(point.y));
 }
   
 export {};
